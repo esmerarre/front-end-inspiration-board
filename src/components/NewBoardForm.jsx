ffac9261ab85c6491590bfa6372ab73392432c79
Esmeralda Arreguin-Martinez <esmerarre@gmail.com>
2026-01-02 12:08:29 -0800

    created Form jsx-css files and completed NewBoardForm.jsx

---
+++ src/components/NewBoardForm.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import './NewBoardForm.css';
+
+const kDefaultsFormState = {
+  title: '',
+  owner: ''
+};
+
+const NewBoardForm = ({createNewBoard}) => {
+  const [formData, setFormData] = useState(kDefaultsFormState); //setting up default state to back the input
+
+  const handleChange = (event) => {
+    const inputName = event.target.name;
+    const inputValue = event.target.value;
+    
+    //as the input changes the state is updated
+    setFormData((formData)=> {
+      return {
+        ...formData,
+        [inputName]: inputValue
+      };
+    });
+  };
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    createNewBoard(formData); //resets the text bar when the form is submitted
+    setFormData(kDefaultsFormState)
+  }
+
+  const makeControlledInput = (inputName) => {
+    return (
+      <input
+        type="text"
+        name={inputName}
+        id={`input-${inputName}`}
+        value={formData[inputName]}
+        onChange={handleChange}
+      />
+    );
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="board-form">
+      <div>
+        <label htmlFor="title">Title: </label>
+        { makeControlledInput('title') }
+      </div>
+      <div>
+        <label htmlFor="description">Owner's Name: </label>
+        { makeControlledInput('description') }
+      </div>
+      <div className="submit-button-wrapper">
+        <input type="submit" value="Create A New Board"/>
+      </div>
+    </form>
+  );
+};
+
+NewBoardForm.propTypes = {
+  createNewBoard: PropTypes.func
+};
+
+export default NewBoardForm;
---
+++ src/components/NewCardForm.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import './NewCardForm.css';
+
+
+const kDefaultsFormState = {
+    message: '',
+};
+
